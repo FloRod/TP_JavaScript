a26716e749210081f30427875d4c5af7f6f6e072
Florian <florod631@gmail.com>
2017-09-28 17:07:20 +0200

    ajout des tests sur les méthodes de services

--- pub-services/services/pubs.services.js
+++ pub-services/services/pubs.services.js
@@ -1,23 +1,45 @@
 var jsonFile = require('../mocks/pubs');
 var moment = require('moment');
-console.log(jsonFile.length);
-console.log(jsonFile[0].name);
+const _ = require('lodash');
+var today = {
+    day: moment().format('dddd'),
+    hour: moment().format('hh')
+};
+
 
 function listAllPubs() {
-    return jsonFile;
-}
+    //if ((typeof jsonFile !== undefined) || (typeof jsonFile[0] !== undefined)) {
+        return jsonFile;
+    // }
+    // else {
+    //     throwErrorJson();
+    // };
+};
 
-function listOpenPubs() {
-    today = {
-        day: moment().format('dddd'),
-        hour: moment().format('hh')
-    };
-
+function listOpenPubs(today) {
     var listPubs = jsonFile.filter(function (item) {
-        return (item.openDays.includes(today.day) && (item.openHours.start <= today.hour && item.openHours.end >= today.hour));
+        return (item.openDays.includes(today)); //&& (item.openHours.start <= today.hour && item.openHours.end >= today.hour));
     })
-   return listPubs;
+    if (! _.isEmpty(listPubs)) {
+  //      console.log(listPubs);
+        return listPubs;
+    }
+    else {
+        throwErrorList();
+    };
+};
+
+
+function throwErrorList() {
+    throw new Error('Erreur : la liste de Pubs est vide');
+};
+
+function throwErrorJson() {
+    throw new Error('Erreur : le fichier json n\'existe pas');
 }
+
+
+//listOpenPubs();
 
 module.exports = {
     listAllPubs: listAllPubs,
--- pub-services/test/pubs.services.spec.js
+++ pub-services/test/pubs.services.spec.js
@@ -1,17 +1,26 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
 var services = require('../services/pubs.services');
 
 describe('pubs.services', function () {
     describe('#listAllPubs()', function () {
-        it('should list throw error', function () {
-            assert.throws(function(){services.listAllPubs}, Error, "");
+        it('should json throw error', function () {
+            //assert.throws(function(){services.listAllPubs()}, Error, 'Erreur : le fichier json n\'existe pas');
         });
 
     });
     describe('#listOpenPubs()', function () {
-        it('should list throw error', function () {
-            assert.throws(function(){services.listOpenPubs}, Error, "");
+        it('listOpenPubs contains elements', function () {
+            expect(services.listOpenPubs("Thursday")).that.not.is.empty;
         });
+        it('listOpenPubs contains elements', function () {
+            expect(function () {services.listOpenPubs("Tursday")}).to.throw();
+        });
+        // it('element listOpenPubs is object', function () {
+        //     expect(services.listOpenPubs()).that.include( ! typeof "object");
+        // });
+        // it('listOpenPubs contains elements', function () {
+        //     expect(services.listOpenPubs()).that.not.is.empty;
+        // });
 
     });
 });
